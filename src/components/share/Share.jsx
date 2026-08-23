402a4c2d14064ae6f679ec9eb83e15f5380897cb
Azka azeem <azkaazeem804@gmail.com>
2026-08-23 10:35:25 +0500

    do file uploader

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,50 +1,79 @@
-import { useContext } from "react";
+import { useContext, useRef, useState } from "react";
 import "./share.css";
 import { PermMedia, Label, Room, EmojiEmotions } from "@mui/icons-material"
 import { AuthContext } from "../../context/AuthContext"
+import axios from "axios";
 
 const Share = () => {
 
-    const {user} = useContext(AuthContext)
+    const { user } = useContext(AuthContext)
     const PF = import.meta.env.VITE_PUBLIC_FOLDER;
-    console.log(user);
-    
-  return (
-    <div className="share">
-        <div className="shareWrapper">
-            <div className="shareTop">
-                <img className="shareProfileImg" src={user.profilePicture ? PF + user.profilePicture : PF + "person/noAvatar.jpg"} alt=""/>
-                <input type="text" placeholder={"What's in your mind" + user.username + "?"} className="shareInput"/>
-            </div>
-            <hr className="shareHr"/>
-            <div className="shareBottom">
-                <div className="shareOptions">
-                    <div className="shareOption">
-                        <PermMedia htmlColor="tomato" className="shareIcon"/>
-                        <span className="shareOptionText">Photo or Video</span>
-                    </div>
+    const desc = useRef();
+    const [file, setFile] = useState(null);
 
-                    <div className="shareOption">
-                        <Label htmlColor="blue" className="shareIcon"/>
-                        <span className="shareOptionText">Tag</span>
-                    </div>
+    const submitHandler = (e) => {
+        e.preventDefault()
+        const newPost = {
+            userId: user._id,
+            desc: desc.current.value
+        };
 
-                    <div className="shareOption">
-                        <Room htmlColor="green" className="shareIcon"/>
-                        <span className="shareOptionText">Location</span>
-                    </div>
+        if (file) {
+            const data = new FormData();
+            const filename = Date.now() + file.name;
+            data.append("file", file);
+            data.append("name", filename);
+            
+        }
 
-                    <div className="shareOption">
-                        <EmojiEmotions htmlColor="goldenrod" className="shareIcon"/>
-                        <span className="shareOptionText">Feelings</span>
-                    </div>
+
+
+        try {
+            axios.post("/posts", newPost)
+        } catch (err) {
+
+        }
+    }
+
+    // console.log(user);
+
+    return (
+        <div className="share">
+            <div className="shareWrapper">
+                <div className="shareTop">
+                    <img className="shareProfileImg" src={user.profilePicture ? PF + user.profilePicture : PF + "person/noAvatar.jpg"} alt="profile" ref={desc} />
+                    <input type="text" placeholder={"What's in your mind " + user.username + "?"} className="shareInput" />
                 </div>
-                <button className="shareButton">Share</button>
-            </div>
-        </div>
+                <hr className="shareHr" />
+                <form className="shareBottom" onSubmit={submitHandler}>
+                    <div className="shareOptions">
+                        <label htmlFor="file" className="shareOption">
+                            <PermMedia htmlColor="tomato" className="shareIcon" />
+                            <span className="shareOptionText">Photo or Video</span>
+                            <input style={{ display: "none" }} type="file" id="file" accept=".png, .jpeg, .jpg" onChange={(e) => {setFile(e.target.files[0])}} />
+                        </label>
 
-    </div>
-  )
+                        <div className="shareOption">
+                            <Label htmlColor="blue" className="shareIcon" />
+                            <span className="shareOptionText">Tag</span>
+                        </div>
+
+                        <div className="shareOption">
+                            <Room htmlColor="green" className="shareIcon" />
+                            <span className="shareOptionText">Location</span>
+                        </div>
+
+                        <div className="shareOption">
+                            <EmojiEmotions htmlColor="goldenrod" className="shareIcon" />
+                            <span className="shareOptionText">Feelings</span>
+                        </div>
+                    </div>
+                    <button className="shareButton" type="submit">Share</button>
+                </form>
+            </div>
+
+        </div>
+    )
 }
 
 export default Share;
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,23 +3,31 @@ import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
     user: {
-        "_id": "6a6eebf8b19b454046a134e0",
-        "username": "Azka1",
-        "email": "azka1@gmail.com",
-        "password": "$2b$10$neuYLoW.FY3px2/WhbayCeO.Ologgq5C3HFowCOWVI1NG3fTStAyK",
-        "profilePicture": "",
-        "coverPicture": "",
-        "followers": [],
-        "followings": [],
-        "isAdmin": false,
-        "createdAt": {"$date": "2026-08-02T07:04:24.907Z"},
-        "updatedAt": {"$date": "2026-08-02T08:24:08.675Z"},
-        "__v": 0,
-        "desc": "my name is Azka.. It's my 1st ID.",
-        "city": "Lahore",
-        "from:": "Pakistan",
-        "relationship": "1"
-    },
+  "_id": {
+    "$oid": "6a6eebf8b19b454046a134e0"
+  },
+  "username": "Azka1",
+  "email": "azka1@gmail.com",
+  "password": "$2b$10$neuYLoW.FY3px2/WhbayCeO.Ologgq5C3HFowCOWVI1NG3fTStAyK",
+  "profilePicture": "person/11.jpg",
+  "coverPicture": "",
+  "followers": [
+    "6a6eebd4b19b454046a134df"
+  ],
+  "followings": [],
+  "isAdmin": false,
+  "createdAt": {
+    "$date": "2026-08-02T07:04:24.907Z"
+  },
+  "updatedAt": {
+    "$date": "2026-08-21T04:44:30.150Z"
+  },
+  "__v": 0,
+  "desc": "my name is Azka.. It's my 1st ID.",
+  "city": "Lahore",
+  "from:": "Pakistan",
+  "relationship": "1"
+},
     isFetching: false,
     error: false,
 };
