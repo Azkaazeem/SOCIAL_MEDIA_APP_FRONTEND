d4a1db6425534062cf30bf3c2ae1371d1c2ff3be
Azka azeem <azkaazeem804@gmail.com>
2026-08-24 11:43:16 +0500

    change static user to dinamic

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,24 +2,7 @@ import { createContext, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
-    user: {
-  "_id": "6a6eebf8b19b454046a134e0",
-  "username": "Azka1",
-  "email": "azka1@gmail.com",
-  "password": "$2b$10$neuYLoW.FY3px2/WhbayCeO.Ologgq5C3HFowCOWVI1NG3fTStAyK",
-  "profilePicture": "person/11.jpg",
-  "coverPicture": "",
-  "followers": [],
-  "followings": [],
-  "isAdmin": false,
-  "createdAt": "2026-08-02T07:04:24.907Z",
-  "updatedAt": "2026-08-21T04:44:30.150Z",
-  "__v": 0,
-  "desc": "my name is Azka.. It's my 1st ID.",
-  "city": "Lahore",
-  "from:": "Pakistan",
-  "relationship": "1"
-},
+    user: null,
     isFetching: false,
     error: false,
 };
